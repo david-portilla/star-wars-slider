f77c5e220f014644d50b447cca4d363e40235ff5
David Portilla <david_portilla@hotmail.com>
2020-06-30 13:20:27 -0500

    add select

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,14 @@
+import { select } from './helpers'
 import Fetch from './components/Fetch'
+import Select from './components/Select'
 
 /*eslint-disable */
-let pullData = new Fetch()
+if (select('.c-slider')) {
+  let pullData = new Fetch()
+}
+
+// create custom select
+if (select('.c-select')) {
+  let customSelect = new Select()
+}
 /*eslint-enable */
